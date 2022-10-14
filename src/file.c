603d936a8d3a8205728c64b0bf73e3b60e789abb
José Guilherme <jose.guilherme.cr.bh@proton.me>
2022-10-14 20:49:41 -0300

    Little refactor in file reading code.

--- src/file.c
+++ src/file.c
@@ -7,18 +7,24 @@
 int
 read_file_from_stdin(struct file *file, uint32_t capacity)
 {
+    const size_t total = (size_t)capacity + 1;
+
     file->size = 0;
-    file->buffer = malloc(capacity + 1);
+    file->buffer = malloc(total);
     if (!file->buffer)
         return -1;
 
-    while (file->size < capacity && !feof(stdin))
-        file->buffer[file->size++] = (char)fgetc(stdin);
+    char c = (char)fgetc(stdin);
+    while (!feof(stdin) && file->size < capacity) {
+        file->buffer[file->size++] = c;
+        c = (char)fgetc(stdin);
+    }
     file->buffer[file->size] = '\0';
 
     // Removing <CR><LF> from files.
     char *ending = NULL;
     if (strstr(file->buffer, "\r\n")) {
+        fputs("Processing \\r\\n...\n", ERR_STREAM);
         ending = strstr(file->buffer, "\r\n");
         while (ending) {
             ending[0] = '\n';
